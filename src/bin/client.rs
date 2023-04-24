b1cbe85fa5d51792a82746ef6c51ec893415684b
Ishan Dongol <ishandongol@gmail.com>
2023-04-24 19:55:35 +0545

    Message Passing

---
+++ src/bin/client.rs
@@ -0,0 +1,69 @@
+use tokio::sync::{mpsc, oneshot};
+
+use bytes::Bytes;
+use mini_redis::client;
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        value: Bytes,
+        resp: Responder<()>,
+    },
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel::<Command>(32);
+    let tx2 = tx.clone();
+    // Establish a connection to the server
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    let _ = resp.send(res);
+                }
+                Command::Set { key, value, resp } => {
+                    let res = client.set(&key, value).await;
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    });
+    // Spawn two tasks, one gets a key, the other sets a key
+    let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+            resp: resp_tx,
+        };
+
+        tx.send(cmd).await.unwrap();
+
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            value: "bar".into(),
+            resp: resp_tx,
+        };
+        tx2.send(cmd).await.unwrap();
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+}
